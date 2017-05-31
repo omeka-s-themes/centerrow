8094928e67b9a829e9a2f011e1d4203b4fd1344c
Kim Nguyen <design@kimisgold.com>
2017-05-31 16:55:16 -0400

    Use search form partial.

--- asset/js/centerrow.js
+++ asset/js/centerrow.js
@@ -1,5 +1,7 @@
 (function($) {
     $(document).ready(function() {
+        $('#search-form').addClass('closed');
+
         $('.search-toggle').click(function() {
             $('#search-form').toggleClass('closed').toggleClass('open');
             if ($('#search-form').hasClass('open')) {
