b103773a4d8cabd854efa9ffb48f29bb491c0a3b
Kim Nguyen <design@kimisgold.com>
2020-10-30 16:02:05 -0400

    Only show gallery pager for items with multiple visual media attachments. (#35)

--- asset/js/centerrow.js
+++ asset/js/centerrow.js
@@ -9,39 +9,5 @@
                 $('#query').focus();
             }
         });
-
-        if ($.isFunction($.fn.lightSlider)) {
-            $('#itemfiles, .item-with-metadata .lightgallery').lightSlider({
-                mode: 'fade',
-                autoWidth: true,
-                adaptiveHeight:false,
-                gallery:true,
-                item:1,
-                loop:true,
-                thumbItem:15,
-                slideMargin:0,
-                enableDrag: false,
-                currentPagerPosition:'middle',
-                onSliderLoad: function(el) {
-                    el.lightGallery({
-                        selector: '#itemfiles .media, .item-with-metadata .lightgallery .media',
-                        download: false,
-                        zoom: true,
-                        youtubePlayerParams: {
-                            modestbranding: 1,
-                            showinfo: 0,
-                            rel: 0,
-                            controls: 1
-                        },
-                        vimeoPlayerParams: {
-                            byline : 0,
-                            portrait : 0,
-                            color : 'A90707'
-                        },
-                    });
-                }
-            });
-        }
-
     });
 })(jQuery)
