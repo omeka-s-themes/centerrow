0ef68234318b06edea05ddfbd0f8648fea281481
Kim Nguyen <design@kimisgold.com>
2021-10-15 17:12:26 -0400

    Begin implementing adobe accessible menu.

--- asset/js/centerrow.js
+++ asset/js/centerrow.js
@@ -1,4 +1,63 @@
+if (!CenterRow) {
+    var CenterRow = {};
+}
+
 (function($) {
+
+    CenterRow.megaMenu = function (menuSelector, customMenuOptions) {
+        if (typeof menuSelector === 'undefined') {
+            menuSelector = 'header nav';
+        }
+
+        var menuOptions = {
+            /* prefix for generated unique id attributes, which are required
+             to indicate aria-owns, aria-controls and aria-labelledby */
+            uuidPrefix: "accessible-megamenu",
+
+            /* css class used to define the megamenu styling */
+            menuClass: "nav-menu",
+
+            /* css class for a top-level navigation item in the megamenu */
+            topNavItemClass: "nav-item",
+
+            /* css class for a megamenu panel */
+            panelClass: "sub-nav",
+
+            /* css class for a group of items within a megamenu panel */
+            panelGroupClass: "sub-nav-group",
+
+            /* css class for the hover state */
+            hoverClass: "hover",
+
+            /* css class for the focus state */
+            focusClass: "focus",
+
+            /* css class for the open state */
+            openClass: "open"
+        };
+
+        $.extend(menuOptions, customMenuOptions);
+
+        $(menuSelector).accessibleMegaMenu(menuOptions);
+    };
+
+    CenterRow.mobileMenu = function() {
+        $('header nav li ul').each(function() {
+            var childMenu = $(this);
+            var subnavToggle = $('<button type="button" class="sub-nav-toggle" aria-label="Show subnavigation"></button>');
+            subnavToggle.click(function() {
+                $(this).parent('.parent').toggleClass('open');
+            });
+            childMenu.parent().addClass('parent');
+            childMenu.addClass('sub-nav').before(subnavToggle);
+        });
+
+        $('.menu-button').click( function(e) {
+            e.preventDefault();
+            $('header nav > ul').toggleClass('open');
+        });
+    };
+
     $(document).ready(function() {
 
         $('#search-form').addClass('closed');
